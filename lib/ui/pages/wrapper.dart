c0c3cc70ed7d552a48912c1f1b94b51de6f16ba1
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2022-10-22 13:18:15 +0700

    created new files

---
+++ lib/ui/pages/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
