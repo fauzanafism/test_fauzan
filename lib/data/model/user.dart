550974076893ca2051c8b22931f4c2e306c4a804
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2022-10-22 13:44:27 +0700

    added user model

---
+++ lib/data/model/user.dart
@@ -0,0 +1,86 @@
+// To parse this JSON data, do
+//
+//     final userResult = userResultFromJson(jsonString);
+
+import 'dart:convert';
+
+UserResult userResultFromJson(String str) =>
+    UserResult.fromJson(json.decode(str));
+
+String userResultToJson(UserResult data) => json.encode(data.toJson());
+
+class UserResult {
+  UserResult({
+    required this.id,
+    required this.page,
+    required this.perPage,
+    required this.totalrecord,
+    required this.totalPages,
+    required this.data,
+  });
+
+  final String id;
+  final int page;
+  final int perPage;
+  final int totalrecord;
+  final int totalPages;
+  final List<Datum> data;
+
+  factory UserResult.fromJson(Map<String, dynamic> json) => UserResult(
+        id: json["\u0024id"],
+        page: json["page"],
+        perPage: json["per_page"],
+        totalrecord: json["totalrecord"],
+        totalPages: json["total_pages"],
+        data: List<Datum>.from(json["data"].map((x) => Datum.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "\u0024id": id,
+        "page": page,
+        "per_page": perPage,
+        "totalrecord": totalrecord,
+        "total_pages": totalPages,
+        "data": List<dynamic>.from(data.map((x) => x.toJson())),
+      };
+}
+
+class Datum {
+  Datum({
+    required this.id,
+    required this.datumId,
+    required this.name,
+    required this.email,
+    required this.profilepicture,
+    required this.location,
+    required this.createdat,
+  });
+
+  final String id;
+  final int datumId;
+  final String name;
+  final String email;
+  final String profilepicture;
+  final String location;
+  final DateTime createdat;
+
+  factory Datum.fromJson(Map<String, dynamic> json) => Datum(
+        id: json["\u0024id"],
+        datumId: json["id"],
+        name: json["name"],
+        email: json["email"],
+        profilepicture: json["profilepicture"],
+        location: json["location"],
+        createdat: DateTime.parse(json["createdat"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "\u0024id": id,
+        "id": datumId,
+        "name": name,
+        "email": email,
+        "profilepicture": profilepicture,
+        "location": location,
+        "createdat": createdat.toIso8601String(),
+      };
+}
