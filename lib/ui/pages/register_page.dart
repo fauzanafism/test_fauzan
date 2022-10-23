681a42ee4eeb0ae190eea94f756f22dec4791403
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2022-10-23 14:35:04 +0700

    created basic ui of RegisterPage

---
+++ lib/ui/pages/register_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:test_fauzan/ui/common/style.dart';
+import 'package:test_fauzan/ui/common/widget.dart';
+import 'package:test_fauzan/ui/pages/login_page.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+  static const String route = '/register_page';
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        const Image(image: AssetImage("assets/images/base_splash.png")),
+        Center(
+          child: Container(
+            width: 279,
+            height: 400,
+            padding: const EdgeInsets.all(18),
+            decoration: BoxDecoration(
+                color: Style.mainColor,
+                borderRadius: BorderRadius.circular(30)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  "Register",
+                  style: GoogleFonts.mulish(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                CustomTextField(
+                    hintText: 'Name',
+                    isObscure: false,
+                    controller: nameController),
+                CustomTextField(
+                    hintText: 'Email',
+                    isObscure: false,
+                    controller: emailController),
+                CustomTextField(
+                    hintText: 'Password',
+                    isObscure: true,
+                    controller: passController),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Style.secondaryColor),
+                  child: Text(
+                    'Register',
+                    style: GoogleFonts.mulish(
+                        fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                ),
+                const Divider(),
+                TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, LoginPage.route);
+                    },
+                    child: Text(
+                      'Login',
+                      style: GoogleFonts.mulish(color: Colors.white),
+                    ))
+              ],
+            ),
+          ),
+        )
+      ]),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passController.dispose();
+    nameController.dispose();
+  }
+}
