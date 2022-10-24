862b093aa0ac3f1f307eba3311a9f827f2902af0
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2022-10-24 15:27:40 +0700

    added enum, register function, and others new obj

--- lib/provider/user_account_provider.dart
+++ lib/provider/user_account_provider.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:test_fauzan/data/model/user_account.dart';
 
+import '../data/api/api_service.dart';
+
+enum RegisterState { loading, failed, success }
+
 class UserAccountProvider extends ChangeNotifier {
-  UserAccountProvider() {
+  final ApiService apiService;
+
+  UserAccountProvider({required this.apiService}) {
     getLoginInfo();
   }
 
@@ -17,8 +23,34 @@ class UserAccountProvider extends ChangeNotifier {
     prefs.setBool('isLogin', _isLogin);
   }
 
+  String _message = '';
+  String get message => _message;
+  
   bool _isLogin = false;
   bool get isLogin => _isLogin;
 
+  late RegisterState _registerState;
+  RegisterState get registerState => _registerState;
+
   late UserAccount _user;
+
+  Future<dynamic> register(String name, String email, String password) async {
+    try {
+      notifyListeners();
+      final user = await apiService.register(name, email, password);
+      if (user.data == null) {
+        _registerState = RegisterState.loading
+        notifyListeners();
+        return _message = 'Empty data';
+      } else {
+        // _resultState = ResultState.hasData;
+        // notifyListeners();
+        // return _restoList = resto;
+      }
+    } catch (_) {
+      // _resultState = ResultState.error;
+      // notifyListeners();
+      // return _message = 'No internet connection';
+    }
+  }
 }
