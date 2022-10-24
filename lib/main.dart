15a72a8fae2ae83a2862179d24c76eb6b119688b
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2022-10-24 14:28:30 +0700

    changed title app

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         )
       ],
       child: MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Test Fauzan App',
         initialRoute: Wrapper.route,
         routes: {
           Wrapper.route: (context) => const Wrapper(),
