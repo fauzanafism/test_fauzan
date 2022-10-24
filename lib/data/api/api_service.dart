47e43441143099549c2e16888662324a614a1b12
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2022-10-24 14:21:40 +0700

    refactored code

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -1,39 +1,39 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:test_fauzan/data/model/register_login.dart';
-import 'package:test_fauzan/data/model/user.dart';
+import 'package:test_fauzan/data/model/user_account.dart';
+import 'package:test_fauzan/data/model/list_user.dart';
 
 class ApiService {
   static const String _baseUrl = "http://restapi.adequateshop.com";
 
-  Future<RegisterLogin> register(
+  Future<UserAccount> register(
       String name, String email, String password) async {
     final response = await http.post(
         Uri.parse("$_baseUrl/api/authaccount/registration"),
         body: {"name": name, "email": email, "password": password});
     if (response.statusCode == 200) {
-      return RegisterLogin.fromJson(json.decode(response.body));
+      return UserAccount.fromJson(json.decode(response.body));
     } else {
       throw Exception('Failed to register');
     }
   }
 
-  Future<RegisterLogin> login(String email, String password) async {
+  Future<UserAccount> login(String email, String password) async {
     final response = await http.post(
         Uri.parse("$_baseUrl/api/authaccount/login"),
         body: {"email": email, "password": password});
     if (response.statusCode == 200) {
-      return RegisterLogin.fromJson(json.decode(response.body));
+      return UserAccount.fromJson(json.decode(response.body));
     } else {
       throw Exception('Failed to register');
     }
   }
 
-  Future<UserResult> getListUser() async {
+  Future<ListUser> getListUser() async {
     final response = await http.get(Uri.parse("${_baseUrl}api/users?page=1"));
     if (response.statusCode == 200) {
-      return UserResult.fromJson(json.decode(response.body));
+      return ListUser.fromJson(json.decode(response.body));
     } else {
       throw Exception('Failed to load');
     }
