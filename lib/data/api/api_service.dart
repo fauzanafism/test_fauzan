4356c4d03e55ee10de3e4a054afa2fb15537e7cc
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2022-10-24 09:10:40 +0700

    added register and login http post function

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -1,10 +1,34 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:test_fauzan/data/model/register_login.dart';
 import 'package:test_fauzan/data/model/user.dart';
 
 class ApiService {
-  static const String _baseUrl = "http://restapi.adequateshop.com/";
+  static const String _baseUrl = "http://restapi.adequateshop.com";
+
+  Future<RegisterLogin> register(
+      String name, String email, String password) async {
+    final response = await http.post(
+        Uri.parse("$_baseUrl/api/authaccount/registration"),
+        body: {"name": name, "email": email, "password": password});
+    if (response.statusCode == 200) {
+      return RegisterLogin.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to register');
+    }
+  }
+
+  Future<RegisterLogin> login(String email, String password) async {
+    final response = await http.post(
+        Uri.parse("$_baseUrl/api/authaccount/login"),
+        body: {"email": email, "password": password});
+    if (response.statusCode == 200) {
+      return RegisterLogin.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to register');
+    }
+  }
 
   Future<UserResult> getListUser() async {
     final response = await http.get(Uri.parse("${_baseUrl}api/users?page=1"));
