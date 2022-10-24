7e8ca9e69fa4071fbd1094f147d7964f2baedf40
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2022-10-24 14:27:49 +0700

    added providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:test_fauzan/provider/list_user_provider.dart';
+import 'package:test_fauzan/provider/user_account_provider.dart';
 import 'package:test_fauzan/ui/pages/login_page.dart';
 import 'package:test_fauzan/ui/pages/register_page.dart';
 import 'package:test_fauzan/ui/pages/wrapper.dart';
@@ -12,14 +15,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      initialRoute: Wrapper.route,
-      routes: {
-        Wrapper.route: (context) => const Wrapper(),
-        RegisterPage.route: (context) => const RegisterPage(),
-        LoginPage.route: (context) => const LoginPage(),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<UserAccountProvider>(
+          create: (_) => UserAccountProvider(),
+        ),
+        ChangeNotifierProvider<ListUserProvider>(
+          create: (_) => ListUserProvider(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        initialRoute: Wrapper.route,
+        routes: {
+          Wrapper.route: (context) => const Wrapper(),
+          RegisterPage.route: (context) => const RegisterPage(),
+          LoginPage.route: (context) => const LoginPage(),
+        },
+      ),
     );
   }
 }
