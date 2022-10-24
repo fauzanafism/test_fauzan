9e4ae98c545579efec3c2a8423eb95cc1ce45953
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2022-10-24 18:53:15 +0700

    added LoginProvider

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:test_fauzan/data/api/api_service.dart';
 import 'package:test_fauzan/provider/home_provider.dart';
+import 'package:test_fauzan/provider/login_provider.dart';
 import 'package:test_fauzan/provider/register_provider.dart';
 import 'package:test_fauzan/ui/pages/home_page.dart';
 import 'package:test_fauzan/ui/pages/login_page.dart';
@@ -22,8 +23,11 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider<RegisterProvider>(
           create: (_) => RegisterProvider(apiService: ApiService()),
         ),
+        ChangeNotifierProvider<LoginProvider>(
+          create: (_) => LoginProvider(apiService: ApiService()),
+        ),
         ChangeNotifierProvider<HomeProvider>(
-          create: (_) => HomeProvider(),
+          create: (_) => HomeProvider(apiService: ApiService()),
         )
       ],
       child: MaterialApp(
