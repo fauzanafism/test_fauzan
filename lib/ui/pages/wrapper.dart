353089a860a7c88e666ba6019d4d2bdce88d434a
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2022-10-24 14:25:10 +0700

    added route

--- lib/ui/pages/wrapper.dart
+++ lib/ui/pages/wrapper.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
+  static const String route = '/';
 
   @override
   Widget build(BuildContext context) {
