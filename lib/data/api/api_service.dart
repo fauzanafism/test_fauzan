2ef4a7aca41b58d83ff07180cb81949e105a2bf6
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2022-10-25 10:50:40 +0700

    added named & optional parameter for getList

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -37,8 +37,9 @@ class ApiService {
     }
   }
 
-  Future<ListUser> getListUser(String token) async {
-    final response = await http.get(Uri.parse("${_baseUrl}api/users?page=1"),
+  Future<ListUser> getListUser(
+      {required String token, String page = '1'}) async {
+    final response = await http.get(Uri.parse("$_baseUrl/api/users?page=$page"),
         headers: {'Authorization': 'Bearer $token'});
     if (response.statusCode == 200) {
       return ListUser.fromJson(json.decode(response.body));
