cd8cdc6a79df48ec784fd0de3d74ca4889d5c584
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2022-10-25 09:34:03 +0700

    adjusted return type of register function

--- lib/provider/register_provider.dart
+++ lib/provider/register_provider.dart
@@ -30,19 +30,20 @@ class RegisterProvider extends ChangeNotifier {
   late UserAccount _user;
   UserAccount get user => _user;
 
-  Future<dynamic> register(String name, String email, String password) async {
+  Future<UserAccount?> register(
+      String name, String email, String password) async {
     try {
       final user = await apiService.register(name, email, password);
       if (user.data == null) {
+        _message = 'Failed';
         notifyListeners();
-        return _message = 'Failed';
       } else {
-        notifyListeners();
         return _user = user;
       }
     } catch (e) {
+      _message = '$e';
       notifyListeners();
-      return _message = '$e';
     }
+    return null;
   }
 }
