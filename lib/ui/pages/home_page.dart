1fd7c1a3da6c8242d184139be24b5ba141fd3833
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2022-10-25 21:50:05 +0700

    added WillPopScope widget for navigate back

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -13,139 +13,145 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-              constraints: const BoxConstraints.expand(),
-              child: const Image(
-                image: AssetImage('assets/images/base_home.png'),
-                fit: BoxFit.cover,
-              )),
-          Consumer<ConnectionProvider>(builder: (context, connection, _) {
-            return Consumer<HomeProvider>(
-              builder: (context, state, _) {
-                if (connection.connectionState == DataState.hasConnection) {
-                  if (state.userListState == UserListState.loading) {
-                    return Stack(
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.popUntil(context, ModalRoute.withName('/'));
+        return true;
+      },
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Container(
+                constraints: const BoxConstraints.expand(),
+                child: const Image(
+                  image: AssetImage('assets/images/base_home.png'),
+                  fit: BoxFit.cover,
+                )),
+            Consumer<ConnectionProvider>(builder: (context, connection, _) {
+              return Consumer<HomeProvider>(
+                builder: (context, state, _) {
+                  if (connection.connectionState == DataState.hasConnection) {
+                    if (state.userListState == UserListState.loading) {
+                      return Stack(
+                        children: [
+                          SafeArea(child: homeAppBar(state)),
+                          const Center(
+                            child: CircularProgressIndicator(),
+                          ),
+                        ],
+                      );
+                    } else if (state.userListState == UserListState.success) {
+                      return SafeArea(
+                        child: Column(
+                          children: [
+                            homeAppBar(state),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 8.0, vertical: 20),
+                                child: ListView.builder(
+                                  itemCount: state.list.data.length,
+                                  itemBuilder: (context, index) {
+                                    var list = state.list.data[index];
+                                    return Padding(
+                                      padding: const EdgeInsets.all(8),
+                                      child: Card(
+                                        child: ListTile(
+                                          leading: ClipRRect(
+                                            child: Image.network(
+                                              list.profilepicture,
+                                              width: 50,
+                                            ),
+                                          ),
+                                          title: Text(
+                                            list.name,
+                                            style: GoogleFonts.mulish(
+                                                fontWeight: FontWeight.w600),
+                                          ),
+                                          subtitle: Text(
+                                            list.location,
+                                            style: GoogleFonts.mulish(),
+                                          ),
+                                          onTap: () {
+                                            AwesomeDialog(
+                                                context: context,
+                                                title:
+                                                    '${list.name} - ${list.location}',
+                                                titleTextStyle:
+                                                    GoogleFonts.mulish(
+                                                        fontSize: 25,
+                                                        fontWeight:
+                                                            FontWeight.w600),
+                                                width: 300,
+                                                desc: list.email,
+                                                descTextStyle:
+                                                    GoogleFonts.mulish(),
+                                                customHeader: ClipRRect(
+                                                  child: Image.network(
+                                                    list.profilepicture,
+                                                    width: 100,
+                                                  ),
+                                                ))
+                                              ..show();
+                                          },
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    } else {
+                      return Center(
+                        child: Text(state.message),
+                      );
+                    }
+                  } else {
+                    return const Center(
+                      child: Text('No internet connection'),
+                    );
+                  }
+                },
+              );
+            }),
+            Positioned(
+                bottom: 0,
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  width: MediaQuery.of(context).size.width,
+                  child: Consumer<HomeProvider>(builder: (context, state, _) {
+                    return Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        SafeArea(child: homeAppBar(state)),
-                        const Center(
-                          child: CircularProgressIndicator(),
+                        Visibility(
+                          visible: (state.page == 1) ? false : true,
+                          child: FloatingActionButton(
+                            backgroundColor: Colors.green,
+                            onPressed: () {
+                              state.page -= 1;
+                              state.getList();
+                            },
+                            child: const Icon(Icons.navigate_before),
+                          ),
+                        ),
+                        FloatingActionButton(
+                          backgroundColor: Colors.green,
+                          onPressed: () {
+                            state.page += 1;
+                            state.getList();
+                          },
+                          child: const Icon(Icons.navigate_next),
                         ),
                       ],
                     );
-                  } else if (state.userListState == UserListState.success) {
-                    return SafeArea(
-                      child: Column(
-                        children: [
-                          homeAppBar(state),
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 8.0, vertical: 20),
-                              child: ListView.builder(
-                                itemCount: state.list.data.length,
-                                itemBuilder: (context, index) {
-                                  var list = state.list.data[index];
-                                  return Padding(
-                                    padding: const EdgeInsets.all(8),
-                                    child: Card(
-                                      child: ListTile(
-                                        leading: ClipRRect(
-                                          child: Image.network(
-                                            list.profilepicture,
-                                            width: 50,
-                                          ),
-                                        ),
-                                        title: Text(
-                                          list.name,
-                                          style: GoogleFonts.mulish(
-                                              fontWeight: FontWeight.w600),
-                                        ),
-                                        subtitle: Text(
-                                          list.location,
-                                          style: GoogleFonts.mulish(),
-                                        ),
-                                        onTap: () {
-                                          AwesomeDialog(
-                                              context: context,
-                                              title:
-                                                  '${list.name} - ${list.location}',
-                                              titleTextStyle:
-                                                  GoogleFonts.mulish(
-                                                      fontSize: 25,
-                                                      fontWeight:
-                                                          FontWeight.w600),
-                                              width: 300,
-                                              desc: list.email,
-                                              descTextStyle:
-                                                  GoogleFonts.mulish(),
-                                              customHeader: ClipRRect(
-                                                child: Image.network(
-                                                  list.profilepicture,
-                                                  width: 100,
-                                                ),
-                                              ))
-                                            ..show();
-                                        },
-                                      ),
-                                    ),
-                                  );
-                                },
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  } else {
-                    return Center(
-                      child: Text(state.message),
-                    );
-                  }
-                } else {
-                  return const Center(
-                    child: Text('No internet connection'),
-                  );
-                }
-              },
-            );
-          }),
-          Positioned(
-              bottom: 0,
-              child: Container(
-                padding: const EdgeInsets.all(10),
-                width: MediaQuery.of(context).size.width,
-                child: Consumer<HomeProvider>(builder: (context, state, _) {
-                  return Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Visibility(
-                        visible: (state.page == 1) ? false : true,
-                        child: FloatingActionButton(
-                          backgroundColor: Colors.green,
-                          onPressed: () {
-                            state.page -= 1;
-                            state.getList();
-                          },
-                          child: const Icon(Icons.navigate_before),
-                        ),
-                      ),
-                      FloatingActionButton(
-                        backgroundColor: Colors.green,
-                        onPressed: () {
-                          state.page += 1;
-                          state.getList();
-                        },
-                        child: const Icon(Icons.navigate_next),
-                      ),
-                    ],
-                  );
-                }),
-              ))
-        ],
+                  }),
+                ))
+          ],
+        ),
       ),
     );
   }
