36a5d9ace975ef7cc09e1784af0fe545ac16b6b6
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2022-10-25 12:57:21 +0700

    added more if condition, adjusted ui

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -10,15 +10,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
       body: Stack(
         children: [
           Container(
               constraints: const BoxConstraints.expand(),
               child: const Image(
-                image: AssetImage('assets/images/base_login_home.png'),
+                image: AssetImage('assets/images/base_home.png'),
                 fit: BoxFit.cover,
               )),
           Consumer<HomeProvider>(
@@ -27,32 +24,37 @@ class HomePage extends StatelessWidget {
                 return const Center(
                   child: CircularProgressIndicator(),
                 );
-              } else {
-                return ListView.builder(
-                  itemCount: state.list.data.length,
-                  itemBuilder: (context, index) {
-                    var list = state.list.data[index];
-                    return Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Card(
-                        child: ListTile(
-                          leading: ClipRRect(
-                            child: Image(
-                              image: NetworkImage(list.profilepicture),
+              } else if (state.userListState == UserListState.success) {
+                return SafeArea(
+                  child: ListView.builder(
+                    itemCount: state.list.data.length,
+                    itemBuilder: (context, index) {
+                      var list = state.list.data[index];
+                      return Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Card(
+                          child: ListTile(
+                            leading: Image.network(
+                              list.profilepicture,
+                              width: 100,
+                            ),
+                            title: Text(
+                              list.name,
+                              style: GoogleFonts.mulish(),
+                            ),
+                            subtitle: Text(
+                              list.location,
+                              style: GoogleFonts.mulish(),
                             ),
                           ),
-                          title: Text(
-                            list.name,
-                            style: GoogleFonts.mulish(),
-                          ),
-                          subtitle: Text(
-                            list.location,
-                            style: GoogleFonts.mulish(),
-                          ),
                         ),
-                      ),
-                    );
-                  },
+                      );
+                    },
+                  ),
+                );
+              } else {
+                return Center(
+                  child: Text(state.message),
                 );
               }
             },
