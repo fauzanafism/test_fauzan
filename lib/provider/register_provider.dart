5b2f96f36dd5ca722245e8f62019f2ae2a1c93c3
fauzanafism <fauzan_nm@apps.ipb.ac.id>
2022-10-25 09:13:31 +0700

    removed unused enum RegisterState

--- lib/provider/register_provider.dart
+++ lib/provider/register_provider.dart
@@ -4,8 +4,6 @@ import 'package:test_fauzan/data/model/user_account.dart';
 
 import '../data/api/api_service.dart';
 
-enum RegisterState { loading, failed, success, error }
-
 class RegisterProvider extends ChangeNotifier {
   final ApiService apiService;
 
@@ -29,28 +27,20 @@ class RegisterProvider extends ChangeNotifier {
   String _message = '';
   String get message => _message;
 
-  late RegisterState _registerState;
-  RegisterState get registerState => _registerState;
-
   late UserAccount _user;
   UserAccount get user => _user;
 
   Future<dynamic> register(String name, String email, String password) async {
     try {
-      _registerState = RegisterState.loading;
-      notifyListeners();
       final user = await apiService.register(name, email, password);
       if (user.data == null) {
-        _registerState = RegisterState.failed;
         notifyListeners();
         return _message = 'Failed';
       } else {
-        _registerState = RegisterState.success;
         notifyListeners();
         return _user = user;
       }
     } catch (e) {
-      _registerState = RegisterState.error;
       notifyListeners();
       return _message = '$e';
     }
